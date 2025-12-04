61b6ba9e176b4c48afa4b87b33d25d09b3b69a17
ryutaroM <arcticmonkey326@gmail.com>
2025-12-05 00:33:52 +0900

    feat: add and

---
+++ and.go
@@ -0,0 +1,26 @@
+package goflow
+
+import "context"
+
+type And[T any] struct {
+	left  Predicator[T]
+	right Predicator[T]
+}
+
+
+func (a And[T]) Test(ctx context.Context, input T) (bool, error) {
+	leftResult, err := a.left.Test(ctx, input)
+	if err != nil {
+		return false, err
+	}
+
+	if !leftResult {
+		return false, nil
+	}
+
+	return a.right.Test(ctx, input)
+}
+
+func AndPredicator[T any](left, right Predicator[T]) Predicator[T] {
+	return And[T]{left: left, right: right}
+}
