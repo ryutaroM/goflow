d79364fd9b8510459973cc16f3c2d55da880905d
ryutaroM <arcticmonkey326@gmail.com>
2025-12-06 12:36:34 +0900

    docs: add OrPredicator example

---
+++ examples/or/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ryutaroM/goflow"
+)
+
+type trueTester struct{}
+
+func (p trueTester) Test(ctx context.Context, input int) (bool, error) {
+	return true, nil
+}
+
+type falseTester struct{}
+
+func (p falseTester) Test(ctx context.Context, input int) (bool, error) {
+	return false, nil
+}
+
+type doubleFlow struct{}
+
+func (f doubleFlow) Process(ctx context.Context, input int) (int, error) {
+	return input * 2, nil
+}
+
+type addTenFlow struct{}
+
+func (f addTenFlow) Process(ctx context.Context, input int) (int, error) {
+	return input + 10, nil
+}
+
+func main() {
+	pred := goflow.OrPredicator(
+		trueTester{},
+		falseTester{},
+	)
+
+	result, err := goflow.NewItem(context.Background(), 4, nil).
+		Branch(
+			pred,
+			doubleFlow{},
+			addTenFlow{},
+		).Result()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	fmt.Println("Result: ", result) // Should print: Result:  8
+}
