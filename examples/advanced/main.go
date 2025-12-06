80199b5273c202dce6c228ce42028a2f1a693666
ryutaroM <arcticmonkey326@gmail.com>
2025-12-06 13:50:49 +0900

    chore: add advanced example

---
+++ examples/advanced/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ryutaroM/goflow"
+)
+
+type isGreaterThan5 struct{}
+
+func (p isGreaterThan5) Test(ctx context.Context, input int) (bool, error) {
+	return input > 5, nil
+}
+
+type isGreaterThan15 struct{}
+
+func (p isGreaterThan15) Test(ctx context.Context, input int) (bool, error) {
+	return input > 15, nil
+}
+
+type addTenFlow struct{}
+
+func (f addTenFlow) Process(ctx context.Context, input int) (int, error) {
+	return input + 10, nil
+}
+
+type subtractFiveFlow struct{}
+
+func (f subtractFiveFlow) Process(ctx context.Context, input int) (int, error) {
+	return input - 5, nil
+}
+
+func main() {
+	b := func(i goflow.Item[int]) goflow.Item[int] {
+		return i.Branch(
+			goflow.AndPredicator(
+				goflow.NotPredicator(isGreaterThan15{}),
+				isGreaterThan5{},
+			),
+			addTenFlow{},
+			subtractFiveFlow{},
+		)
+	}
+
+	result7, err := b(goflow.NewItem(context.Background(), 7, nil)).Result()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	result15, err := b(goflow.NewItem(context.Background(), 15, nil)).Result()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	fmt.Println("-----Nested Branching Result-----")
+	fmt.Println("Result: ", result7)  // Should print: Result:  17
+	fmt.Println("Result: ", result15) // Should print: Result:  25
+}
